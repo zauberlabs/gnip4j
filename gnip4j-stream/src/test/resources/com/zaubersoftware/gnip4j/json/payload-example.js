a606a1df1e01851a292f7d7c2c09f616e9005c0c
Guido Marucci Blas <guido@zaubersoftware.com>
2011-04-28 16:06:04 -0300

    Adds a gnip payload example

---
+++ gnip4j-stream/src/test/resources/com/zaubersoftware/gnip4j/json/payload-example.js
@@ -0,0 +1,89 @@
+var response = 
+{
+		  "verb": "post",
+		  "location": {
+		    "country_code": "United States",
+		    "displayName": "Downtown, Boulder",
+		    "objectType": "place",
+		    "geo": {
+		      "coordinates": [
+		        [
+		          [
+		            -105.295899,
+		            40.013625
+		          ],
+		          [
+		            -105.271683,
+		            40.013625
+		          ],
+		          [
+		            -105.271683,
+		            40.020434
+		          ],
+		          [
+		            -105.295899,
+		            40.020434
+		          ]
+		        ]
+		      ],
+		      "type": "Polygon"
+		    },
+		    "streetAddress": null,
+		    "link": "http://api.twitter.com/1/geo/id/9be2ad0f7f091494.json"
+		  },
+		  "postedTime": "2011-01-03T17:48:08+00:00",
+		  "body": "For some reason I find it pretty easy to remember to write '2011' rather than '2010' this year.",
+		  "actor": {
+		    "location": {
+		      "displayName": "Boulder, CO",
+		      "objectType": "place"
+		    },
+		    "postedTime": "2008-03-28T22:14:17+00:00",
+		    "displayName": "Nick howard",
+		    "preferredUsername": "baroquebobcat",
+		    "utcOffset": -25200,
+		    "objectType": "person",
+		    "statusesCount": 2354,
+		    "languages": [
+		      "en"
+		    ],
+		    "listedCount": 18,
+		    "links": [
+		      {
+		        "href": "http://blog.baroquebobcat.com",
+		        "rel": "me"
+		      }
+		    ],
+		    "friendsCount": 329,
+		    "followersCount": 219,
+		    "summary": "Am interested in CS:AI, Web, identity Also I like to fence, sing and play video games ",
+		    "link": "http://twitter.com/baroquebobcat",
+		    "image": "http://a1.twimg.com/profile_images/1154659940/tw_profile_normal.jpg"
+		  },
+		  "objectType": "activity",
+		  "id": "tag:search.twitter.com,2005:21986181948379136",
+		  "geo": {
+		    "coordinates": [
+		      40.0191219,
+		      -105.2746654
+		    ],
+		    "type": "Point"
+		  },
+		  "link": "http://twitter.com/baroquebobcat/statuses/21986181948379136",
+		  "generator": {
+		    "displayName": "geo tweeter",
+		    "link": "http://geo-tweeter.heroku.com/"
+		  },
+		  "object": {
+		    "postedTime": "2011-01-03T17:48:08+00:00",
+		    "id": "tag:search.twitter.com,2005:21986181948379136",
+		    "objectType": "note",
+		    "link": "http://twitter.com/baroquebobcat/statuses/21986181948379136",
+		    "summary": "For some reason I find it pretty easy to remember to write '2011' rather than '2010' this year."
+		  },
+		  "provider": {
+		    "displayName": "Twitter",
+		    "objectType": "service",
+		    "link": "http://www.twitter.com"
+		  }
+		};
