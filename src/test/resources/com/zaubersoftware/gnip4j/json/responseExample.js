bf154695df46dc8c88b10d5dca5df2229f3cc368
Guido Marucci Blas <guido@zaubersoftware.com>
2011-04-28 13:05:07 -0300

    Adds a Gnip JSON response example

---
+++ src/test/resources/com/zaubersoftware/gnip4j/json/responseExample.js
@@ -0,0 +1,62 @@
+var response = {
+	"id" : "tag:search.twitter.com,2005:63355839691493376",
+	"body" : "Voltando do supermercado, eu passei por um bar perto de casa que tava passando Simpsons na tv. =OOO Preciso beber uma coca-cola nesse lugar!",
+	"verb" : "post",
+	"link" : "http://twitter.com/Cinthia_Ribeiro/statuses/63355839691493376",
+	"generator" : {
+		"link" : "http://www.tweetdeck.com",
+		"displayName" : "TweetDeck"
+	},
+	"postedTime" : "2011-04-27T21:36:23.000Z",
+	"provider" : {
+		"link" : "http://www.twitter.com",
+		"displayName" : "Twitter",
+		"objectType" : "service"
+	},
+	"object" : {
+		"summary" : "Voltando do supermercado, eu passei por um bar perto de casa que tava passando Simpsons na tv. =OOO Preciso beber uma coca-cola nesse lugar!",
+		"id" : "object:search.twitter.com,2005:63355839691493376",
+		"link" : "http://twitter.com/Cinthia_Ribeiro/statuses/63355839691493376",
+		"postedTime" : "2011-04-27T21:36:23.000Z",
+		"objectType" : "note"
+	},
+	"actor" : {
+		"summary" : "Não tão complicada demais mas nem tão simples assim.",
+		"friendsCount" : 262,
+		"location" : {
+			"displayName" : "Fortaleza/CE/Brasil",
+			"objectType" : "place"
+		},
+		"link" : "http://www.twitter.com/Cinthia_Ribeiro",
+		"postedTime" : "2009-07-06T22:54:03.000Z",
+		"image" : "http://a0.twimg.com/profile_images/1286986451/C_pia_de_25032011_002__normal.jpg",
+		"links" : [ {
+			"rel" : "me",
+			"href" : "http://cinthiards.blogspot.com/"
+		} ],
+		"listedCount" : 67,
+		"id" : "id:twitter.com:54363025",
+		"languages" : [ "en" ],
+		"followersCount" : 849,
+		"utcOffset" : "-10800",
+		"preferredUsername" : "Cinthia_Ribeiro",
+		"displayName" : "Cinthia",
+		"statusesCount" : 33879,
+		"objectType" : "person"
+	},
+	"twitter_entities" : {
+		"urls" : [],
+		"user_mentions" : [],
+		"hashtags" : []
+	},
+	"objectType" : "activity",
+	"gnip" : {
+		"language" : {
+			"value" : "pt"
+		},
+		"matching_rules" : [ {
+			"value" : "coca-cola",
+			"tag" : null
+		} ]
+	}
+};
