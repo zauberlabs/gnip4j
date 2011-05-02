76392105ae419fbf306c9d4c9512b059420c4ad5
Guido Marucci Blas <guido@zaubersoftware.com>
2011-05-02 14:08:55 -0300

    Adds another twitter payload example

---
+++ core/src/test/resources/twitter-payload-example.js
@@ -0,0 +1,74 @@
+var data = {
+	"id" : "tag:search.twitter.com,2005:65095667961446400",
+	"inReplyTo" : {
+		"link" : "http://twitter.com/RodAllen12/statuses/65090846445404160"
+	},
+	"body" : "@RodAllen12  I would trade AJax, Schlereth, and Coke for Granderson today, contracts and all.  #stillmytiger",
+	"verb" : "post",
+	"link" : "http://twitter.com/Tigerdog_1/statuses/65095667961446400",
+	"generator" : {
+		"link" : "http://twitter.com",
+		"displayName" : "web"
+	},
+	"postedTime" : "2011-05-02T16:49:51.000Z",
+	"provider" : {
+		"link" : "http://www.twitter.com",
+		"displayName" : "Twitter",
+		"objectType" : "service"
+	},
+	"object" : {
+		"summary" : "@RodAllen12  I would trade AJax, Schlereth, and Coke for Granderson today, contracts and all.  #stillmytiger",
+		"id" : "object:search.twitter.com,2005:65095667961446400",
+		"link" : "http://twitter.com/Tigerdog_1/statuses/65095667961446400",
+		"postedTime" : "2011-05-02T16:49:51.000Z",
+		"objectType" : "note"
+	},
+	"actor" : {
+		"summary" : "Tigers, Red Wings, Michigan, and USA!",
+		"friendsCount" : 112,
+		"location" : {
+			"displayName" : "In the OC, California",
+			"objectType" : "place"
+		},
+		"link" : "http://www.twitter.com/Tigerdog_1",
+		"postedTime" : "2010-06-29T18:41:20.000Z",
+		"image" : "http://a2.twimg.com/profile_images/1142771332/Cobb_normal.jpg",
+		"links" : [ {
+			"rel" : "me",
+			"href" : null
+		} ],
+		"listedCount" : 6,
+		"id" : "id:twitter.com:161005599",
+		"languages" : [ "en" ],
+		"followersCount" : 79,
+		"utcOffset" : "-28800",
+		"preferredUsername" : "Tigerdog_1",
+		"displayName" : "Tiger Dog",
+		"statusesCount" : 1858,
+		"objectType" : "person"
+	},
+	"twitter_entities" : {
+		"hashtags" : [ {
+			"text" : "stillmytiger",
+			"indices" : [ 95, 108 ]
+		} ],
+		"urls" : [],
+		"user_mentions" : [ {
+			"indices" : [ 0, 11 ],
+			"id_str" : "41523902",
+			"screen_name" : "RodAllen12",
+			"name" : "Rod Allen",
+			"id" : 41523902
+		} ]
+	},
+	"objectType" : "activity",
+	"gnip" : {
+		"language" : {
+			"value" : "en"
+		},
+		"matching_rules" : [ {
+			"value" : "coke",
+			"tag" : null
+		} ]
+	}
+};
