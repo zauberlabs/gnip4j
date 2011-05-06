2b3f66685a34a6947fd113d60509751b89fa58d4
Guido Marucci Blas <guido@zaubersoftware.com>
2011-05-06 15:47:59 -0300

    Adds support for more JSON elements in the Activity Object

--- http/src/test/java/com/zaubersoftware/gnip4j/http/JSONDeserializationTest.java
+++ http/src/test/java/com/zaubersoftware/gnip4j/http/JSONDeserializationTest.java
@@ -17,9 +17,12 @@ package com.zaubersoftware.gnip4j.http;
 
 import static org.junit.Assert.*;
 
+import java.io.InputStream;
+import java.io.Reader;
 import java.io.StringReader;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.AnnotationIntrospector;
 import org.codehaus.jackson.map.DeserializationConfig;
@@ -28,6 +31,7 @@ import org.codehaus.jackson.xc.JaxbAnnotationIntrospector;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import com.zaubersoftware.gnip4j.api.model.Activity;
 import com.zaubersoftware.gnip4j.api.model.MatchingRules;
 
@@ -44,17 +48,17 @@ public final class JSONDeserializationTest {
     
     @Before
     public void setUp() throws Exception {
-//        final InputStream is = getClass().getClassLoader().getResourceAsStream(
-//        "/gnip4j-http/src/test/resources/com/zaubersoftware/gnip4j/payload/twitter-payload-example.js");
-//        
-        final StringReader is = new StringReader("{\"id\":\"tag:search.twitter.com,2005:66216309981061120\",\"inReplyTo\":{\"link\":\"http://twitter.com/heirtoblair/statuses/66216007370424320\"},\"body\":\"@heirtoblair Seriously? That program kicks my ass. All for a coke? You go girl!\",\"verb\":\"post\",\"link\":\"http://twitter.com/lotsOspermies/statuses/66216309981061120\",\"generator\":{\"link\":\"http://twitter.com\",\"displayName\":\"web\"},\"postedTime\":\"2011-05-05T19:02:53.000Z\",\"provider\":{\"link\":\"http://www.twitter.com\",\"displayName\":\"Twitter\",\"objectType\":\"service\"},\"object\":{\"summary\":\"@heirtoblair Seriously? That program kicks my ass. All for a coke? You go girl!\",\"id\":\"object:search.twitter.com,2005:66216309981061120\",\"link\":\"http://twitter.com/lotsOspermies/statuses/66216309981061120\",\"postedTime\":\"2011-05-05T19:02:53.000Z\",\"objectType\":\"note\"},\"actor\":{\"summary\":\"My vag will never be the same after spawning 3 boys! I'm a wife, PPD ass kickah, blogger, snarky biznitch & a wanna be SAHM... \",\"friendsCount\":331,\"location\":{\"displayName\":\"Florida\",\"objectType\":\"place\"},\"link\":\"http://www.twitter.com/lotsOspermies\",\"postedTime\":\"2010-06-17T14:34:03.000Z\",\"image\":\"http://a1.twimg.com/profile_images/1336079729/me_normal.jpg\",\"links\":[{\"rel\":\"me\",\"href\":\"http://www.spermiestyle.com/\"}],\"listedCount\":40,\"id\":\"id:twitter.com:156653884\",\"languages\":[\"en\"],\"followersCount\":557,\"utcOffset\":\"-18000\",\"preferredUsername\":\"lotsOspermies\",\"displayName\":\"Pamela Gold \",\"statusesCount\":4554,\"objectType\":\"person\"},\"twitter_entities\":{\"urls\":[],\"user_mentions\":[{\"indices\":[0,12],\"screen_name\":\"heirtoblair\",\"id_str\":\"19663219\",\"name\":\"Heir to Blair Blog\",\"id\":19663219}],\"hashtags\":[]},\"objectType\":\"activity\",\"gnip\":{\"language\":{\"value\":\"en\"},\"matching_rules\":[{\"value\":\"coke\",\"tag\":null}]}}\n");
+        
+        final InputStream is = getClass().getClassLoader().getResourceAsStream(
+        "com/zaubersoftware/gnip4j/payload/payload-example.js");
         final ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         final AnnotationIntrospector introspector = new JaxbAnnotationIntrospector();
         mapper.setDeserializationConfig(
                 mapper.getDeserializationConfig().withAnnotationIntrospector(introspector));
         mapper.setSerializationConfig(
                 mapper.getSerializationConfig().withAnnotationIntrospector(introspector));
+        //TODO This configuration should be removed once the JSON is fully supported
+        mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         final JsonParser parser = mapper.getJsonFactory().createJsonParser(is);
         activity = parser.readValueAs(Activity.class);
     }
