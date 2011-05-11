499fb228f61d6d315b510601bd0afeb451d8e869
Juan F. Codagnone <juan@zaubersoftware.com>
2011-05-11 12:25:15 -0300

    Fix in the mapping for activities.

--- http/src/test/java/com/zaubersoftware/gnip4j/http/JSONDeserializationTest.java
+++ http/src/test/java/com/zaubersoftware/gnip4j/http/JSONDeserializationTest.java
@@ -18,8 +18,13 @@ package com.zaubersoftware.gnip4j.http;
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.List;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.apache.commons.io.IOUtils;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.map.AnnotationIntrospector;
 import org.codehaus.jackson.map.DeserializationConfig;
@@ -39,15 +44,13 @@ import com.zaubersoftware.gnip4j.api.model.MatchingRules;
  * @since May 5, 2011
  */
 public final class JSONDeserializationTest {
-
-    private Activity activity;
+    private ObjectMapper mapper;
+    private JAXBContext ctx; 
     
     @Before
     public void setUp() throws Exception {
         
-        final InputStream is = getClass().getClassLoader().getResourceAsStream(
-        "com/zaubersoftware/gnip4j/payload/payload-example.js");
-        final ObjectMapper mapper = new ObjectMapper();
+        mapper = new ObjectMapper();
         final AnnotationIntrospector introspector = new JaxbAnnotationIntrospector();
         mapper.setDeserializationConfig(
                 mapper.getDeserializationConfig().withAnnotationIntrospector(introspector));
@@ -55,27 +58,51 @@ public final class JSONDeserializationTest {
                 mapper.getSerializationConfig().withAnnotationIntrospector(introspector));
         //TODO This configuration should be removed once the JSON is fully supported
         mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        final JsonParser parser = mapper.getJsonFactory().createJsonParser(is);
-        activity = parser.readValueAs(Activity.class);
+        ctx = JAXBContext.newInstance(Activity.class.getPackage().getName());
     }
 
     @Test
     public void testGetGnip() throws Exception {
-        assertNotNull(activity.getGnip());
+        final InputStream is = getClass().getClassLoader().getResourceAsStream(
+        "com/zaubersoftware/gnip4j/payload/payload-example.js");
+        try  {
+            final JsonParser parser = mapper.getJsonFactory().createJsonParser(is);
+            final Activity activity = parser.readValueAs(Activity.class);
+            
+            assertNotNull(activity.getGnip());
+            assertNotNull(activity.getGnip().getLanguage());
+            assertEquals("en", activity.getGnip().getLanguage().getValue());
+            final List<MatchingRules> matchingRules = activity.getGnip().getMatchingRules();
+            assertNotNull(matchingRules);
+            assertEquals(1, matchingRules.size());
+            assertEquals("coke", matchingRules.get(0).getValue());
+            assertEquals(null, matchingRules.get(0).getTag());
+        } finally {
+            is.close();
+        }
     }
     
     @Test
-    public void testGetLanguage() throws Exception {
-        assertNotNull(activity.getGnip().getLanguage());
-        assertEquals("en", activity.getGnip().getLanguage().getValue());
-    }
-    
-    @Test
-    public void testGetMatchingRules() throws Exception {
-        final List<MatchingRules> matchingRules = activity.getGnip().getMatchingRules();
-        assertNotNull(matchingRules);
-        assertEquals(1, matchingRules.size());
-        assertEquals("coke", matchingRules.get(0).getValue());
-        assertEquals(null, matchingRules.get(0).getTag());
+    public void testNPE() throws Exception {
+        final InputStream is = getClass().getClassLoader().getResourceAsStream(
+            "com/zaubersoftware/gnip4j/payload/payload-twitter-entities.js");
+        final InputStream expectedIs = getClass().getClassLoader().getResourceAsStream(
+            "com/zaubersoftware/gnip4j/payload/payload-twitter-entities.xml");
+        
+        try {
+            final String json = IOUtils.toString(is);
+            final JsonParser parser = mapper.getJsonFactory().createJsonParser(json);
+            final Activity activity = parser.readValueAs(Activity.class);
+            final StringWriter w = new StringWriter();
+            mapper.getJsonFactory().createJsonGenerator(w).writeObject(activity);
+            
+            final Marshaller o = ctx.createMarshaller();
+            o.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            StringWriter ww = new StringWriter();
+            o.marshal(activity, ww);
+            assertEquals(IOUtils.toString(expectedIs), ww.toString());
+        } finally {
+            is.close();
+        }
     }
 }
---
+++ http/src/test/resources/com/zaubersoftware/gnip4j/payload/payload-twitter-entities.js
@@ -0,0 +1,91 @@
+{
+	"id" : "tag:search.twitter.com,2005:68323667712540672",
+	"inReplyTo" : {
+		"link" : "http://twitter.com/SashaHalima/statuses/68321671823634432"
+	},
+	"body" : "GREAT!-&gt; Coke asks people to tweet their #1thing that would change the world http://bit.ly/itsC30 ~via @SashaHalima @BrandRepublic #in",
+	"verb" : "post",
+	"link" : "http://twitter.com/LeslyCardec/statuses/68323667712540672",
+	"generator" : {
+		"link" : "http://twitter.com",
+		"displayName" : "web"
+	},
+	"postedTime" : "2011-05-11T14:36:46.000Z",
+	"provider" : {
+		"link" : "http://www.twitter.com",
+		"displayName" : "Twitter",
+		"objectType" : "service"
+	},
+	"object" : {
+		"summary" : "GREAT!-&gt; Coke asks people to tweet their #1thing that would change the world http://bit.ly/itsC30 ~via @SashaHalima @BrandRepublic #in",
+		"id" : "object:search.twitter.com,2005:68323667712540672",
+		"link" : "http://twitter.com/LeslyCardec/statuses/68323667712540672",
+		"postedTime" : "2011-05-11T14:36:46.000Z",
+		"objectType" : "note"
+	},
+	"actor" : {
+		"summary" : "? #PR Mgr for @SFNGroup & family of brands | ? #PR #SM #Marketing #Blogs | New Mom | Blogger | Wanna-Be Super Woman | http://www.linkedin.com/in/leslycardec ?\r\n",
+		"friendsCount" : 1849,
+		"location" : {
+			"displayName" : "561, Florida",
+			"objectType" : "place"
+		},
+		"link" : "http://www.twitter.com/LeslyCardec",
+		"postedTime" : "2008-05-07T14:27:40.000Z",
+		"image" : "http://a2.twimg.com/profile_images/1254438524/BaerLe_normal.jpg",
+		"links" : [ {
+			"rel" : "me",
+			"href" : "http://www.sfngroup.com"
+		}],
+		"listedCount" : 388,
+		"id" : "id:twitter.com:14686987",
+		"languages" : [ "en" ],
+		"followersCount" : 1552,
+		"utcOffset" : "-18000",
+		"preferredUsername" : "LeslyCardec",
+		"displayName" : "Lesly Baer-Cardec",
+		"statusesCount" : 4117,
+		"objectType" : "person"
+	},
+	"twitter_entities" : {
+		"hashtags" : [ {
+			"text" : "1thing",
+			"indices" : [ 44, 51 ]
+		}, {
+			"text" : "in",
+			"indices" : [ 134, 137 ]
+		} ],
+		"urls" : [ {
+			"expanded_url" : "http://google.com",
+			"indices" : [ 80, 100 ],
+			"url" : "http://bit.ly/itsC30"
+		} ],
+		"user_mentions" : [ {
+			"indices" : [ 106, 118 ],
+			"id_str" : "17799367",
+			"screen_name" : "SashaHalima",
+			"name" : "Sasha H. Muradali ",
+			"id" : 17799367
+		}, {
+			"indices" : [ 119, 133 ],
+			"id_str" : "7087112",
+			"screen_name" : "BrandRepublic",
+			"name" : "BrandRepublic",
+			"id" : 7087112
+		} ]
+	},
+	"objectType" : "activity",
+	"gnip" : {
+		"language" : {
+			"value" : "en"
+		},
+		"matching_rules" : [ {
+			"value" : "coke",
+			"tag" : null
+		} ],
+		"urls" : [ {
+			"url" : "http://bit.ly/itsC30",
+			"expanded_url" : "http://wallblog.co.uk/2011/05/11/coke-asks-world-to-tweet-the-1thing-that-would-change-the-world/"
+		} ]
+	}
+}
