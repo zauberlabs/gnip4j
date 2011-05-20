d223cc7aec58b3733df3f3ecfc14408819d8b43c
Juan F. Codagnone <juan@zaubersoftware.com>
2011-05-20 19:56:55 -0300

    more test

---
+++ core/src/test/java/com/zaubersoftware/gnip4j/api/StreamNotificationAdapterTest.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2011 Zauber S.A. <http://www.zaubersoftware.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zaubersoftware.gnip4j.api;
+
+import org.junit.Test;
+
+import com.zaubersoftware.gnip4j.api.exception.GnipException;
+import com.zaubersoftware.gnip4j.api.exception.TransportGnipException;
+import com.zaubersoftware.gnip4j.api.model.Activity;
+
+
+/**
+ * Test for {@link StreamNotification}
+ *
+ * @author Juan F. Codagnone
+ * @since May 20, 2011
+ */
+public class StreamNotificationAdapterTest {
+
+    /** nothing to do! */
+    @Test
+    public final void testNull() {
+        final StreamNotification n = new StreamNotificationAdapter() {
+            @Override
+            public void notify(final Activity activity, final GnipStream stream) {
+            }
+        };
+        n.notifyConnectionError(null);
+        n.notifyReConnectionError(null);
+        n.notifyReConnection(-1, -1000);
+    }
+}
