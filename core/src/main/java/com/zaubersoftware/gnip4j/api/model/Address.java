0d47e990bdae0b3b0368bf122d25b1212328b0d7
Juan F. Codagnone <juan@flowics.com>
2015-07-24 14:20:12 -0300

    missing file

---
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/model/Address.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2011-2012 Zauber S.A. <http://www.zaubersoftware.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zaubersoftware.gnip4j.api.model;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ * TODO: Description of the class, Comments in english by default
+ *
+ *
+ * @author Juan F. Codagnone
+ * @since Mar 20, 2015
+ */
+public class Address {
+    // Los Angeles County
+    private String subRegion;
+    // California
+    private String region;
+    // San Fernando
+    private String locality;
+    // US
+    private String countryCode;
+    // United States
+    private String country;
+
+    @JsonIgnore
+    public boolean isOnlyCountry() { 
+        return countryCode != null && region == null && locality == null; 
+    }
+    
+    public String getSubRegion() {
+        return subRegion;
+    }
+
+    public void setSubRegion(final String subRegion) {
+        this.subRegion = subRegion;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(final String region) {
+        this.region = region;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(final String locality) {
+        this.locality = locality;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(final String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+
+}
---
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/model/Location.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2011-2012 Zauber S.A. <http://www.zaubersoftware.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zaubersoftware.gnip4j.api.model;
+
+/**
+ *
+ *
+ * @author Juan F. Codagnone
+ * @since Mar 20, 2015
+ */
+public class Location {
+    // San Fernando, California, United States
+    private String displayName;
+    private Address address;
+    private Geo geo;
+    
+    // place
+    private String objectType;
+    public String getDisplayName() {
+        return displayName;
+    }
+    public void setDisplayName(final String displayName) {
+        this.displayName = displayName;
+    }
+    public Address getAddress() {
+        return address;
+    }
+    public void setAddress(final Address address) {
+        this.address = address;
+    }
+    public Geo getGeo() {
+        return geo;
+    }
+    public void setGeo(final Geo geo) {
+        this.geo = geo;
+    }
+    public String getObjectType() {
+        return objectType;
+    }
+    public void setObjectType(final String objectType) {
+        this.objectType = objectType;
+    }
+}
