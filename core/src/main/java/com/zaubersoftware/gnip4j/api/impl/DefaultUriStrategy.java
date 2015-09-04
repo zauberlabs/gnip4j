d660e6c04f3cac6ec3c902a499bd1f51f3697e80
Juan F. Codagnone <juan@flowics.com>
2015-09-04 16:18:25 -0300

    DefaultUriStrategy: host and schema can be configured

--- core/src/main/java/com/zaubersoftware/gnip4j/api/impl/DefaultUriStrategy.java
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/impl/DefaultUriStrategy.java
@@ -35,15 +35,21 @@ import com.zaubersoftware.gnip4j.api.UriStrategy;
  * @since 11/11/2011
  */
 public final class DefaultUriStrategy implements UriStrategy {
-    public static final String BASE_GNIP_STREAM_URI = "https://stream.gnip.com:443/accounts/%s/publishers/%s/streams/track/%s.json";
-    public static final String BASE_GNIP_RULES_URI = "https://api.gnip.com:443/accounts/%s/publishers/%s/streams/track/%s/rules.json";
+    public static final String DEFAULT_STREAM_URL_BASE  = "https://stream.gnip.com:443";
+    public static final String DEFAULT_RULE_URL_BASE = "https://api.gnip.com:443";
+    public static final String PATH_GNIP_STREAM_URI =  "/accounts/%s/publishers/%s/streams/track/%s.json";
+    public static final String PATH_GNIP_RULES_URI =  "/accounts/%s/publishers/%s/streams/track/%s/rules.json";
+    
+    private String streamUrlBase = DEFAULT_STREAM_URL_BASE;
+    private String ruleUrlBase = DEFAULT_RULE_URL_BASE;
+    
+    
     private final String publisher;
     
     /** Creates the DefaultUriStrategy. */
     public DefaultUriStrategy() {
         this("twitter");
     }
-    
     /** Creates the DefaultUriStrategy. */
     public DefaultUriStrategy(final String publisher) {
         if (publisher == null) {
@@ -62,7 +68,7 @@ public final class DefaultUriStrategy implements UriStrategy {
         }
         
         
-        return URI.create(String.format(BASE_GNIP_STREAM_URI, account.trim(), publisher.trim(), streamName.trim()));
+        return URI.create(String.format(streamUrlBase + PATH_GNIP_STREAM_URI, account.trim(), publisher.trim(), streamName.trim()));
     }
 
     @Override
@@ -74,6 +80,30 @@ public final class DefaultUriStrategy implements UriStrategy {
             throw new IllegalArgumentException("The streamName cannot be null or empty");
         }
         
-        return URI.create(String.format(BASE_GNIP_RULES_URI, account.trim(), publisher.trim(), streamName.trim()));
+        return URI.create(String.format(ruleUrlBase + PATH_GNIP_RULES_URI, account.trim(), publisher.trim(), streamName.trim()));
     }
+    
+    public final String getStreamUrlBase() {
+        return streamUrlBase;
+    }
+    
+    public final void setStreamUrlBase(final String streamUrlBase) {
+        if(streamUrlBase == null) {
+            throw new IllegalArgumentException("streamUrlBase can't be null");
+        }
+        this.streamUrlBase = streamUrlBase;
+    }
+    
+    public final String getRuleUrlBase() {
+        return ruleUrlBase;
+    }
+    
+    public final void setRuleUrlBase(final  String ruleUrlBase) {
+        if(ruleUrlBase == null) {
+            throw new IllegalArgumentException("streamUrlBase can't be null");
+        }
+        this.ruleUrlBase = ruleUrlBase;
+    }
+    
+    
 }
