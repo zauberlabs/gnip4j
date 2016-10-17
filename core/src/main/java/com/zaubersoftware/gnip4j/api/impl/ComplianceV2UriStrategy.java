fd53b87d2f0af2b093f8fdc1a7d2915a84bfd31d
Dennis Lloyd Jr <dennislloydjr@gmail.com>
2016-10-17 10:35:29 -0400

    Support v2 Compliance Stream

---
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/impl/ComplianceV2UriStrategy.java
@@ -0,0 +1,60 @@
+package com.zaubersoftware.gnip4j.api.impl;
+
+import com.zaubersoftware.gnip4j.api.UriStrategy;
+
+import java.net.URI;
+
+public class ComplianceV2UriStrategy implements UriStrategy {
+    public static final String BASE_GNIP_STREAM_URI_V2 = "https://gnip-api.twitter.com/stream/compliance/accounts/%s/publishers/twitter/%s.json?partition=%s";
+
+    private final String baseGnipStreamUri;
+    private final String partition;
+
+    public static final String BASE_GNIP_RULES_URI = "https://gnip-api.twitter.com/rules/powertrack/accounts/%s/publishers/twitter/%s.json";
+
+    public ComplianceV2UriStrategy(final int partition) {
+        if (partition < 1 || partition > 8) {
+            throw new IllegalArgumentException("Partition must be between 1 and 8 for compliance version 2");
+        } else {
+            this.baseGnipStreamUri = BASE_GNIP_STREAM_URI_V2;
+            this.partition = Integer.toString(partition);
+        }
+    }
+
+    @Override
+    public URI createStreamUri(String account, String streamName) {
+        if (account == null || account.trim().isEmpty()) {
+            throw new IllegalArgumentException("The account cannot be null or empty");
+        }
+        if (streamName == null || streamName.trim().isEmpty()) {
+            throw new IllegalArgumentException("The streamName cannot be null or empty");
+        }
+        return URI.create(String.format(baseGnipStreamUri, account.trim(), streamName.trim(), partition));
+    }
+
+    @Override
+    public URI createRulesUri(final String account, final String streamName) {
+        return URI.create(createRulesBaseUrl(account, streamName));
+    }
+
+    @Override
+    public URI createRulesDeleteUri(final String account, final String streamName) {
+        return URI.create(createRulesBaseUrl(account, streamName) + "?_method=delete");
+    }
+
+    @Override
+    public String getHttpMethodForRulesDelete() {
+        return UriStrategy.HTTP_POST;
+    }
+
+    private String createRulesBaseUrl(final String account, final String streamName) {
+        if (account == null || account.trim().isEmpty()) {
+            throw new IllegalArgumentException("The account cannot be null or empty");
+        }
+        if (streamName == null || streamName.trim().isEmpty()) {
+            throw new IllegalArgumentException("The streamName cannot be null or empty");
+        }
+
+        return String.format(BASE_GNIP_RULES_URI, account.trim(), streamName.trim());
+    }
+}
---
+++ core/src/main/java/com/zaubersoftware/gnip4j/api/impl/formats/ComplianceActivityUnmarshaller.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2011-2016 Zauber S.A. <http://flowics.com/>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zaubersoftware.gnip4j.api.impl.formats;
+
+import com.zaubersoftware.gnip4j.api.model.Activity;
+import com.zaubersoftware.gnip4j.api.model.compliance.ComplianceActivity;
+import com.zaubersoftware.gnip4j.api.support.logging.LoggerFactory;
+import com.zaubersoftware.gnip4j.api.support.logging.spi.Logger;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import java.io.IOException;
+
+/**
+ * Translates JSON input from the Compliance v2 stream into instances of Activity.
+ *
+ * @see <a href="http://support.gnip.com/sources/twitter/data_format.html#SamplePayloads">Sample Payloads</a>
+ *
+ * @author Dennis Lloyd Jr
+ * @since Oct 14, 2016
+ */
+public class ComplianceActivityUnmarshaller implements Unmarshaller<Activity> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final ObjectMapper mapper = JsonActivityFeedProcessor.getObjectMapper();
+
+    @Override
+    public final Activity unmarshall(final String s) {
+        try {
+            return mapper.readValue(s, ComplianceActivity.class).toActivity();
+        } catch (final JsonMappingException e) {
+            logger.warn("Failed to parse compliance activity: " + s, e);
+            return null;
+        } catch (final IOException e) {
+            throw new IllegalArgumentException("parsing compliance activity", e);
+        }
+    }
+}
